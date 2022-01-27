d32ab1fe226c165ed5ed4ebb0cfc0b95ce4fa685
Mike Resnik <mikeresnik@gmail.com>
2022-01-27 10:18:34 -0500

    Removes math duplicate code in favor of math jar from resnik.math

--- src/main/kotlin/com/resnik/math/linear/array/ArrayTensorCoordIteratorInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.resnik.math.linear.array
-
-interface ArrayTensorCoordIteratorInterface : Iterator<IntArray> {
-
-    fun numTraversed() : Int
-
-    fun coords() : IntArray
-
-}
--- src/main/kotlin/com/resnik/math/linear/array/ArrayTensorRegion.kt
+++
@@ -1,6 +0,0 @@
-package com.resnik.math.linear.array
-
-import com.resnik.math.linear.array.ArrayTensorIndex
-
-class ArrayTensorRegion (val first : ArrayTensorIndex, val second : ArrayTensorIndex) {
-}
